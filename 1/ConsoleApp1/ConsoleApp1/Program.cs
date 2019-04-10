4cf31e19a8a75a2364104804fd784a12e598d873
Dmitry Kaboskin <dimankab@gmail.com>
2019-04-10 11:40:42 +0300

    'first'

---
+++ 1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("input.txt");
+            string line;
+            int waycount = 0;
+            var size = System.IO.File.ReadAllLines("input.txt").Length;
+            int[,] arr = new int[size, size];
+            int n=0, m = 0, way;
+            while ((line = reader.ReadLine()) != null) //Заполнение массива из файла
+            {
+                foreach(string element in line.Split())
+                {
+                    way = int.Parse(element);
+                    arr[n, m] = way;
+                    m++;
+                }
+                m = 0;
+                n++;
+            }
+            int flag = 0;
+            for(int i = 0; i < arr.GetLength(0); i++) //Подсчет количества путей
+            {
+                for(int j = 0 + flag; j < arr.GetLength(1); j++)
+                {
+                    if(arr[i,j] == 1 && arr[j,i] == 1)
+                    {
+                        waycount++;
+                    }
+                }
+                flag++;
+            }
+            StreamWriter sw = new StreamWriter("output.txt");
+            sw.WriteLine(waycount);
+            sw.Close();
+        }
+    }
+}
---
+++ 3/ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine().ToUpper();
+            Console.WriteLine(text);
+            string[] words = text.Split(new char[] { ' ', ',', '.' }, StringSplitOptions.RemoveEmptyEntries);
+            var dict = new Dictionary<string, int>();
+            var maxcount = 0;
+            string maxword = "";
+            foreach (string word in words)
+            {
+                if(dict.ContainsKey(word))
+                {
+                    if(++dict[word] > maxcount)
+                    {
+                        maxcount = dict[word];
+                        maxword = word;
+                    }
+                }
+                else
+                {
+                    dict.Add(word, 1);
+                }
+            }
+            Console.WriteLine(maxword + '-' + maxcount);
+        }
+    }
+}
